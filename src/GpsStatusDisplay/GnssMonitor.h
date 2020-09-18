3f2537ceb8ccfb5ff6f1bb375bf6e9a945183373
Morten Nielsen <mn@iter.dk>
2020-09-17 22:38:50 -0700

    Move nmea event logic into separate file. Add pushbutton paging + display timeout

---
+++ src/GpsStatusDisplay/GnssMonitor.h
@@ -0,0 +1,213 @@
+
+#ifndef __GnssMonitor_h__
+#define __GnssMonitor_h__
+
+#include "NMEAParser.h"
+
+typedef void (*LocationHandler)(const char *type);
+NMEAParser<5> mParser; 
+static LocationHandler mLocationHandler;
+// State variables
+float mLatitude;
+String mLatIndicator;
+float mLongitude;
+String mLonIndicator;
+float mElevation;
+String mMode;
+float mVerticalError;
+float mHorizontalError;
+String mHdop;
+String mVdop;
+String mPdop;
+int mSats;
+String mGpstime;
+
+void raiseLocationEvent(const char *type)
+{
+  if (mLocationHandler != NULL)
+  {
+     mLocationHandler(type);
+  }
+};
+void handleRMCMessage(void)
+{
+  raiseLocationEvent("RMC");
+};
+void handleGGAMessage(void)
+{
+  float lat;
+  float lon;
+  if (mParser.getArg(1, lat) && mParser.getArg(3, lon))
+  {
+    // Write latitude
+    String strlat;
+    mParser.getArg(1, strlat);
+    mParser.getArg(2, mLatIndicator);
+    mLatitude = strlat.substring(0,2).toFloat() + strlat.substring(2).toFloat() / 60;
+    // Write longitude
+    String strlon;
+    mParser.getArg(3, strlon);
+    mParser.getArg(4, mLonIndicator);
+    mLongitude = strlon.substring(0,3).toFloat() + strlon.substring(3).toFloat() / 60;
+  }
+  else
+  {
+    mLatitude = NAN;
+    mLongitude = NAN;
+  }
+  // Write elevation
+  float el;
+  if(mParser.getArg(8, el))
+  {
+    mElevation = el;
+  }
+  else
+  {
+    mElevation = NAN;
+  }
+  
+  int quality = 0;
+  
+  mParser.getArg(5, quality);
+  if(quality == 0)
+     mMode = "No Fix";
+  else if(quality == 1)
+     mMode = "GPS Fix";
+  else if(quality == 2)
+     mMode = "Differential";
+  else if(quality == 3)
+     mMode = "GPS PPS";
+  else if(quality == 4)
+     mMode = "RTK";
+  else if(quality == 5)
+     mMode = "RTK Float";
+  else if(quality == 6)
+     mMode = "Estimated";
+  else
+     mMode = "---";
+
+  int satCount = 0;
+  mParser.getArg(6, satCount);
+  //writepair("Satellites", String(satCount), 1, display);
+  mSats = satCount;
+  String time;
+  if(mParser.getArg(0, time))
+      mGpstime = time.substring(0,2) + ":" + time.substring(2,4) + ":" + time.substring(4,6);
+  else
+    mGpstime = "---";
+  raiseLocationEvent("GGA");
+};
+
+void handleGSAMessage(void)
+{
+  int count = mParser.argCount();
+  int systemId;
+  if(mParser.getArg(count-1, systemId) && systemId == 1)
+  {
+    if(count > 4 && mParser.getArg(count - 4, mPdop) && mParser.getArg(count - 3, mHdop) && mPdop.length() > 0 && mHdop.length() > 0
+    && mParser.getArg(count - 2, mVdop)) 
+    {
+    }
+    else 
+    {
+       mPdop = "---";
+       mHdop = "---";
+       mVdop = "---";
+    }
+  }
+  raiseLocationEvent("GSA");
+};
+
+void handleGSTMessage(void)
+{
+  float error_lat;
+  float error_lon;
+  
+  if (mParser.getArg(5, error_lat) && mParser.getArg(6, error_lon))
+  {
+    float error = sqrt(error_lat * error_lat + error_lon * error_lon);
+    mHorizontalError = error;
+  }
+  else
+  {
+    String error;
+    mParser.getArg(5, error);
+    mHorizontalError = NAN;
+  }
+  float error_v;
+  if (mParser.getArg(7, error_v))
+  {
+    mVerticalError = error_v;
+  }
+  else
+  {
+    mVerticalError = NAN;
+  }
+  raiseLocationEvent("GST");
+};
+
+void handleGSVMessage(void)
+{
+  //char buf[6];
+  //mParser.getType(buf);  
+};
+
+void unknownCommandMessage()
+{
+  //char buf[6];
+  //mParser.getType(buf);
+  //if(buf == "GNVTG" || buf == "GNGLL)
+   // return;
+  //write(buf, 1, 0);
+};
+
+void errorHandlerMessage()
+{
+  //char buf[6];
+  //mParser.getType(buf);  
+  //write(String(mParser.error()) + " " + buf, 6, 0); 
+};
+
+  void initNmeaParser() 
+  {    
+     mParser.addHandler("GNRMC", handleRMCMessage);
+     mParser.addHandler("GNGGA", handleGGAMessage);
+     mParser.addHandler("GNGST", handleGSTMessage);
+     mParser.addHandler("GNGSA", handleGSAMessage);
+     mParser.addHandler("--GSV", handleGSVMessage);
+     mParser.setDefaultHandler(unknownCommandMessage);
+  };
+  
+  void setLocationHandler(LocationHandler inHandler)
+  {
+    mLocationHandler = inHandler;
+  };
+  
+char buf[1024];
+void readData()
+{
+  int count;
+  if (Serial1.available())
+  {
+    while((count = Serial1.readBytes(buf, 1024)) > 0)
+    {
+      for(int i=0;i<count;i++)
+        mParser << buf[i];
+    }
+  }
+};
+
+  float latitude() { return mLatitude; }
+  String latIndicator() { return mLatIndicator; }
+  float longitude() { return mLongitude; }
+  String lonIndicator() { return mLonIndicator; }
+  float elevation() { return mElevation; }
+  String mode() { return mMode; }
+  float verticalError() { return mVerticalError; }
+  float horizontalError() { return mHorizontalError; }
+  String hdop() { return mHdop; }
+  String vdop() { return mVdop; }
+  String pdop() { return mPdop; }
+  int sats() { return mSats; }
+  String gpstime() { return mGpstime; }
+#endif
--- src/GpsStatusDisplay/GpsStatusDisplay.ino
+++ src/GpsStatusDisplay/GpsStatusDisplay.ino
@@ -13,31 +13,19 @@
 #include <Wire.h>
 #include "SSD1306Wire.h"
 #include "NMEAParser.h"
-NMEAParser<4> parser; 
+#include "GnssMonitor.h"
 bool ledstate;
+bool isDisplayOff;
 
 // Initialize the OLED display using Arduino Wire:
 SSD1306Wire display(0x3c, SDA, SCL);
 int16_t currentDisplay = 0;
 int16_t fontHeight = 10;
 
-// State variables
-float latitude;
-String latIndicator;
-float longitude;
-String lonIndicator;
-float elevation;
-String mode;
-float verticalError;
-float horizontalError;
-String hdop;
-String vdop;
-String pdop;
-int sats;
-String gpstime;
-
 void showDisplay()
 {
+  if(isDisplayOff)
+    return;
   display.clear();
    if(currentDisplay == 0)
       drawPage1();
@@ -49,6 +37,7 @@ void showDisplay()
       drawPage4();
   display.display();
 }
+
 void writepair(const String lefttext, const String righttext, const int row)
 {
   uint16_t y = row*fontHeight;
@@ -57,6 +46,7 @@ void writepair(const String lefttext, const String righttext, const int row)
   display.setTextAlignment(TEXT_ALIGN_RIGHT);
   display.drawString(128, y, righttext);
 }
+
 void writepair(const String lefttext, const float righttext, const int decimals, const int row)
 {
   if(righttext == NAN)
@@ -64,37 +54,40 @@ void writepair(const String lefttext, const float righttext, const int decimals,
   else
     writepair(lefttext, String(righttext, decimals), row);
 }
+
 void drawPage1()
 { 
   display.setFont(ArialMT_Plain_10);
-  writepair("Mode", mode, 0);
-  writepair("Sats",String(sats), 1);
-  if(latitude == NAN)
+  writepair("Mode", mode(), 0);
+  writepair("Sats",String(sats()), 1);
+  if(latitude() == NAN)
     writepair("Lat","---", 2);
   else
-    writepair("Lat",String(latitude, 2) + "*****" + latIndicator, 2);
-  if(longitude == NAN)
+    writepair("Lat",String(latitude(), 2) + "*****" + latIndicator(), 2);
+  if(longitude() == NAN)
     writepair("Lon","---", 3);
   else
-    writepair("Lon",String(longitude, 2) + "*****" + lonIndicator, 3);
+    writepair("Lon",String(longitude(), 2) + "*****" + lonIndicator(), 3);
 
-  if(elevation == NAN)
+  if(elevation() == NAN)
     writepair("MSL","---", 4);
   else
-    writepair("MSL",String(elevation, 3) + "m", 4);
+    writepair("MSL",String(elevation(), 3) + "m", 4);
 
-  writepair("Time",gpstime, 5);
+  writepair("Time",gpstime(), 5);
 }
+
 void drawPage2()
 { 
   display.setFont(ArialMT_Plain_10);
-  writepair("Mode", mode, 0);
-  writepair("Horizontal Error", horizontalError, 3, 1);
-  writepair("Vertical Error", verticalError, 3, 2);
-  writepair("HDOP ", hdop, 3);
-  writepair("VDOP ", vdop, 4);
-  writepair("PDOP ", pdop, 5);
+  writepair("Mode", mode(), 0);
+  writepair("Horizontal Error", horizontalError(), 3, 1);
+  writepair("Vertical Error", verticalError(), 3, 2);
+  writepair("HDOP ", hdop(), 3);
+  writepair("VDOP ", vdop(), 4);
+  writepair("PDOP ", pdop(), 5);
 }
+
 void drawPage3()
 { 
   display.drawCircle(64, 32, 31);
@@ -111,194 +104,73 @@ void drawPage4()
 { 
   display.setFont(ArialMT_Plain_16);
   display.setTextAlignment(TEXT_ALIGN_CENTER);
-  display.drawString(64, 4, String(latitude, 2) + "*****" + latIndicator);
-  display.drawString(64, 24, String(longitude, 2) + "*****" + lonIndicator);
-  display.drawString(64, 44, String(elevation, 3) + "m");
-}
-void handleRMC(void)
-{
-  // Flash LED each time RMC is received
-  if(ledstate)
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  else
-    digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-  ledstate = !ledstate;
-}
-void handleGGA(void)
-{
-  float lat;
-  float lon;
-  if (parser.getArg(1, lat) && parser.getArg(3, lon))
-  {
-    // Write latitude
-    String strlat;
-    parser.getArg(1, strlat);
-    parser.getArg(2, latIndicator);
-    latitude = strlat.substring(0,2).toFloat() + strlat.substring(2).toFloat() / 60;
-    // Write longitude
-    String strlon;
-    parser.getArg(3, strlon);
-    parser.getArg(4, lonIndicator);
-    longitude = strlon.substring(0,3).toFloat() + strlon.substring(3).toFloat() / 60;
-  }
-  else
-  {
-    latitude = NAN;
-    longitude = NAN;
-  }
-  // Write elevation
-  float el;
-  if(parser.getArg(8, el))
-  {
-    elevation = el;
-  }
-  else
-  {
-    elevation = NAN;
-  }
-  
-  int quality = 0;
-  
-  parser.getArg(5, quality);
-  if(quality == 0)
-     mode = "No Fix";
-  else if(quality == 1)
-     mode = "GPS Fix";
-  else if(quality == 2)
-     mode = "Differential";
-  else if(quality == 3)
-     mode = "GPS PPS";
-  else if(quality == 4)
-     mode = "RTK";
-  else if(quality == 5)
-     mode = "RTK Float";
-  else if(quality == 6)
-     mode = "Estimated";
-  else
-     mode = "---";
-
-  int satCount = 0;
-  parser.getArg(6, satCount);
-  //writepair("Satellites", String(satCount), 1, display);
-  sats = satCount;
-  String time;
-  if(parser.getArg(0, time))
-      gpstime = time.substring(0,2) + ":" + time.substring(2,4) + ":" + time.substring(4,6);
-  else
-    gpstime = "---";
-  showDisplay();
+  display.drawString(64, 4, String(latitude(), 2) + "*****" + latIndicator());
+  display.drawString(64, 24, String(longitude(), 2) + "*****" + lonIndicator());
+  display.drawString(64, 44, String(elevation(), 3) + "m");
 }
 
-void handleGSA(void)
+void onLocation(const char *type)
 {
-  int count = parser.argCount();
-  int systemId;
-  if(parser.getArg(count-1, systemId) && systemId == 1)
-  {
-    if(count > 4 && parser.getArg(count - 4, pdop) && parser.getArg(count - 3, hdop) && pdop.length() > 0 && hdop.length() > 0
-    && parser.getArg(count - 2, vdop)) 
-    {
-    }
-    else 
-    {
-       pdop = "---";
-       hdop = "---";
-       vdop = "---";
-    }
+  if(type == "RMC")
+  {  
+    // Flash LED each time RMC is received
+    if(ledstate)
+      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    else
+      digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+    ledstate = !ledstate;
   }
-  showDisplay();
+  else {
+    showDisplay();
+  }
 }
-
-void handleGST(void)
-{
-  float error_lat;
-  float error_lon;
-  
-  if (parser.getArg(5, error_lat) && parser.getArg(6, error_lon))
-  {
-    float error = sqrt(error_lat * error_lat + error_lon * error_lon);
-    horizontalError = error;
-  }
-  else
-  {
-    String error;
-    parser.getArg(5, error);
-    horizontalError = NAN;
-  }
-  float error_v;
-  if (parser.getArg(7, error_v))
-  {
-    verticalError = error_v;
-  }
-  else
-  {
-    verticalError = NAN;
-  }
-  showDisplay();
-}
-void unknownCommand()
-{
-  char buf[6];
-  parser.getType(buf);
-  //if(buf == "GNVTG" || buf == "GNGLL)
-   // return;
-  //write(buf, 1, 0);
-}
-
-void errorHandler()
-{
-  char buf[6];
-  parser.getType(buf);
-  
-  //write(String(parser.error()) + " " + buf, 6, 0); 
-  //showDisplay();
-}
+const int buttonPin = 2;
+unsigned long lastButtonPressTime;
 void setup()
 {
     // Initialising the UI will init the display too.
   display.init();  
   display.flipScreenVertically();
   display.clear();
-  
+  setLocationHandler(onLocation);
   Wire.begin();   
   // Page 1
   showDisplay();
-
+  initNmeaParser();
+  lastButtonPressTime = millis();
   Serial1.begin(38400); //38400
   Serial1.setTimeout(10);
   pinMode(LED_BUILTIN, OUTPUT);
-  parser.addHandler("GNRMC", handleRMC);
-  parser.addHandler("GNGGA", handleGGA);
-  parser.addHandler("--GST", handleGST);
-  parser.addHandler("GNGSA", handleGSA);
-  parser.setDefaultHandler(unknownCommand);
-  parser.setErrorHandler(errorHandler);
+  pinMode(buttonPin, INPUT);
 }
-String a;
-char buf[1024];
+int buttonState = 0;
 void loop()
 {
-  int count;
-  
   auto t = millis();
-  int index = (t / 5000) % 4;
-  if(currentDisplay != index)
+  // Turn display off after 30 seconds of not pressing the button
+  if(t - lastButtonPressTime > 30000 && !isDisplayOff)
   {
-    currentDisplay = index;
-    showDisplay();      
+    isDisplayOff = true;
+    display.displayOff();
   }
-  
-  if (Serial1.available()) {
-    while((count = Serial1.readBytes(buf, 1024)) > 0)
+  // Check button state and turn screen back on or flip pages
+  auto newButtonState = digitalRead(buttonPin);
+  if (newButtonState != buttonState && newButtonState == HIGH) {
+    lastButtonPressTime = t;
+    if(isDisplayOff)
     {
-      for(int i=0;i<count;i++)
-        parser << buf[i];
+      isDisplayOff = false;
+      display.displayOn();
     }
-  
-    /*if(ledstate)
-      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    else
-      digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-    ledstate = !ledstate;*/
+    else {
+      currentDisplay++;
+      if(currentDisplay > 3)
+        currentDisplay = 0;
+    }
+    showDisplay();
   }
+  buttonState = newButtonState;
+
+  //Parse serial NMEA data
+  readData();
 }
